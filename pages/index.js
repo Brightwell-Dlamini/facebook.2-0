40d5a496371b4f17e1ffd58fbc54588f33b480be
Brightwell-Dlamini <47980034+Brightwell-Dlamini@users.noreply.github.com>
2022-03-01 22:15:52 +0200

    Login

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
+import { getSession } from "next-auth/react";
 import Head from "next/head";
 import Header from "../components/Header";
+import Login from "../components/Login";
 
-export default function Home() {
+export default function Home({ session }) {
+  if (!session) return <Login />;
   return (
     <div className="">
       <Head>
@@ -12,4 +15,12 @@ export default function Home() {
       <Header />
     </div>
   );
+}
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+  return {
+    props: {
+      session,
+    },
+  };
 }
